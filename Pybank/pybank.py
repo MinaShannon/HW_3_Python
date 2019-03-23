f83c605d317d6f090cab3266192c3ae845612f55
Mina Shannon <will.mt.shannon@gmail.com>
2019-03-23 19:33:05 -0400

    saving progress

--- Pybank/pybank.py
+++ Pybank/pybank.py
@@ -1,2 +1,11 @@
 # PyBank
-if __name__=="__main__"
+#if __name__=="__main__"
+import csv
+import os
+csvpath=os.path.join("..""PyBank.csv")
+with open(csvpath,newline="") as csvfile:
+    csv.reader(csvfile,delimiter=",")
+    csvheader=next(csvfile)
+    months=enumerate(row[0])-1
+    print(months)
+    for rows in csvreader:
